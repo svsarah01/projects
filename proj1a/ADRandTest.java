66fa5bf7bfc3adb6eaa3ddca519476159d108af8
Sarah Varghese <svsarah01@berkeley.edu>
2020-09-14 07:38:46 -0700

    style checker passed

--- proj1a/ADRandTest.java
+++ proj1a/ADRandTest.java
@@ -5,15 +5,13 @@ import java.util.Random;
 public class ADRandTest {
 
     @Test
-    public void Test() {
+    public void test() {
         System.out.println("Running randomized test.");
         ArrayDeque<Integer> ad = new ArrayDeque<Integer>();
         LinkedListDeque<Integer> lld = new LinkedListDeque<>();
         Random rand = new Random(5);
-        //the value 5 is the seed. This means that if I run the code over and over, I get a deterministic result
-        //That makes it much easier to debug, since you can recreate the issue and set a conditional breakpoint accordingly
-        for (int i = 0; i < 200; i++) { //the i == 5 is arbitrary, just wanted it to be readable
-            int randomnum = rand.nextInt(5); //gives a random value between 0 and 1 inclusive
+        for (int i = 0; i < 200; i++) {
+            int randomnum = rand.nextInt(5);
             if (randomnum == 0) {
                 ad.addFirst(i);
                 lld.addFirst(i);
