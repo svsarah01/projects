2142f36f40dba389409c0d852d60fbcbd6bb1675
Sarah Varghese <svsarah01@berkeley.edu>
2020-09-16 08:39:27 -0700

    wrote a test for offByN

--- proj1b/TestOffByN.java
+++ proj1b/TestOffByN.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class TestOffByN {
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestOffByN {
+    static CharacterComparator offByN = new OffByN(5);
+
+    @Test
+    public void testObN() {
+        assertTrue(offByN.equalChars('a', 'f'));
+        assertTrue(offByN.equalChars('f', 'a'));
+        assertFalse(offByN.equalChars('c', 'e'));
+        assertFalse(offByN.equalChars('a', 'a'));
+    }
 }
