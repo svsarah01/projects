77b2e869bf4e88684e6ddd072508ddcccd7c92cd
Sarah Varghese <svsarah01@berkeley.edu>
2020-10-05 14:38:56 -0700

    2 WQUs implemented to prevent backwash

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,20 +2,22 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    boolean [][] grid;
-    WeightedQuickUnionUF wQU;
-    int size;
-    int topSite;
-    int bottomSite;
-    int totalOpen;
+    private boolean [][] grid;
+    private int size;
+    private WeightedQuickUnionUF topSite;
+    private WeightedQuickUnionUF bottomSite;
+    private int totalOpen;
+    private int topNode;
+    private int bottomNode;
 
     public Percolation(int N) { // create N-by-N grid, with all sites initially blocked
         size = N;
         totalOpen = 0;
         grid = new boolean[N][N];
-        topSite = N;
-        bottomSite = N + 1;
-        wQU = new WeightedQuickUnionUF(N*N + 2);
+        topSite = new WeightedQuickUnionUF(N * N + 1);
+        bottomSite = new WeightedQuickUnionUF(N * N + 2);
+        topNode = N * N;
+        bottomNode = (N * N) + 1;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 grid[i][j] = false;
@@ -23,11 +25,11 @@ public class Percolation {
         }
     }
 
-    public int xyTo1D(int row, int col) {
+    private int xyTo1D(int row, int col) {
         return (size * row) + col;
     }
 
-    public void isValid (int i) {
+    private void isValid (int i) {
         if (i < 0 || i >= size) {
             throw new IndexOutOfBoundsException("Not a valid row or column index!");
         }
@@ -41,22 +43,27 @@ public class Percolation {
             totalOpen += 1;
         }
         if (row == 0) {
-            wQU.union(topSite, xyTo1D(row, col));
+            bottomSite.union(topNode, xyTo1D(row, col));
+            topSite.union(topNode, xyTo1D(row, col));
         }
         if (row == size - 1) {
-            wQU.union(bottomSite, xyTo1D(row, col));
+            bottomSite.union(bottomNode, xyTo1D(row, col));
         }
         if (row != size - 1 && grid[row + 1][col]) {
-            wQU.union(xyTo1D(row, col), xyTo1D(row + 1, col));
+            bottomSite.union(xyTo1D(row, col), xyTo1D(row + 1, col));
+            topSite.union(xyTo1D(row, col), xyTo1D(row + 1, col));
         }
         if (row != 0 && grid[row - 1][col]) {
-            wQU.union(xyTo1D(row, col), xyTo1D(row - 1, col));
+            bottomSite.union(xyTo1D(row, col), xyTo1D(row - 1, col));
+            topSite.union(xyTo1D(row, col), xyTo1D(row - 1, col));
         }
         if (col != size - 1 && grid[row][col + 1]) {
-            wQU.union(xyTo1D(row, col), xyTo1D(row, col + 1));
+            bottomSite.union(xyTo1D(row, col), xyTo1D(row, col + 1));
+            topSite.union(xyTo1D(row, col), xyTo1D(row, col + 1));
         }
         if (col != 0 && grid[row][col - 1]) {
-            wQU.union(xyTo1D(row, col), xyTo1D(row, col - 1));
+            bottomSite.union(xyTo1D(row, col), xyTo1D(row, col - 1));
+            topSite.union(xyTo1D(row, col), xyTo1D(row, col - 1));
         }
     }
     public boolean isOpen(int row, int col) { // is the site (row, col) open?
@@ -67,13 +74,13 @@ public class Percolation {
     public boolean isFull(int row, int col) { // is the site (row, col) full?
         isValid(row);
         isValid(col);
-        return wQU.connected(xyTo1D(row, col), topSite);
+        return topSite.connected(topNode, xyTo1D(row, col));
     }
     public int numberOfOpenSites() {  // number of open sites
         return totalOpen;
     }
     public boolean percolates() { // does the system percolate?
-        return wQU.connected(topSite, bottomSite);
+        return bottomSite.connected(topNode, bottomNode);
     }
     public static void main(String[] args) { // use for unit testing (not required, but keep this here for the autograder)
 
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,47 @@
 package hw2;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
+    private int experiments;
+    private int size;
+    private Percolation grid;
+    private double [] x_array;
 
+    public PercolationStats(int N, int T, PercolationFactory pf) { // perform T independent experiments on an N-by-N grid
+        experiments = T;
+        size = N;
+        if (N <= 0 || T < 0) {
+            throw new IllegalArgumentException();
+        }
+        x_array = new double [T];
+        for (int i = 0; i < T; i ++) {
+            grid = pf.make(size);
+            int xi = 0;
+            while (!(grid.percolates())) {
+                int row = StdRandom.uniform(0, size);
+                int col = StdRandom.uniform(0, size);
+                if (grid.isOpen(row, col)) {
+                    continue;
+                }
+                else {
+                    grid.open(row, col);
+                    xi += 1;
+                }
+            }
+            x_array[i] = xi/N;
+        }
+    }
+    public double mean() { // sample mean of percolation threshold
+        return StdStats.mean(x_array);
+    }
+    public double stddev() { // sample standard deviation of percolation threshold
+        return StdStats.stddev(x_array);
+    }
+    public double confidenceLow() { // low endpoint of 95% confidence interval
+        return mean() - (1.96 * stddev() / Math.sqrt(experiments));
+    }
+    public double confidenceHigh() { // high endpoint of 95% confidence interval
+        return mean() + (1.96 * stddev() / Math.sqrt(experiments));
+    }
 }
