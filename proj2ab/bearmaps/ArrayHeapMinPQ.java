7166896fdcfd3b2c70b42b9e15eb22d1ca434933
Sarah Varghese <svsarah01@berkeley.edu>
2020-10-21 17:48:36 -0700

    I think everything is correct???

---
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -0,0 +1,183 @@
+package bearmaps;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.NoSuchElementException;
+
+public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
+    private class PriorityNode {
+        private T item;
+        private double priority;
+        private int index;
+
+        PriorityNode(T i, double p, int x) {
+            item = i;
+            priority = p;
+            index = x;
+        }
+
+        int getIndex() {
+            return this.index;
+        }
+
+        T getItem() {
+            return this.item;
+        }
+
+        Double getPriority() {
+            return this.priority;
+        }
+
+        void setIndex(int x) {
+            this.index = x;
+        }
+
+        void setPriority(double p) {
+            this.priority = p;
+        }
+
+        boolean smaller(PriorityNode other) {
+            if (other == null) {
+                return true;
+            }
+            return this.getPriority() < other.getPriority();
+        }
+    }
+
+    private PriorityNode[] items;
+    private HashMap<T, PriorityNode> itemMap;
+    private int size;
+    private int end;
+
+    public ArrayHeapMinPQ() {
+        items = new ArrayHeapMinPQ.PriorityNode[4];
+        size = 0;
+        end = 1;
+        itemMap = new HashMap<>();
+    }
+
+    private PriorityNode leftChild(PriorityNode node) {
+        if (node.getIndex() * 2 > items.length - 1) {
+            return null;
+        }
+        return items[node.getIndex() * 2];
+    }
+
+    private PriorityNode rightChild(PriorityNode node) {
+        if (node.getIndex() * 2 > items.length - 1) {
+            return null;
+        }
+        return items[(node.getIndex() * 2) + 1];
+    }
+
+    private PriorityNode parent(PriorityNode node) {
+        return items[node.getIndex() / 2];
+    }
+
+    private void resize(int capacity) {
+        PriorityNode[] temp = new ArrayHeapMinPQ.PriorityNode[capacity];
+        for (int i = 0; i <= size; i++) {
+            temp[i] = items[i];
+        }
+        items = temp;
+    }
+
+    private void swap(PriorityNode n1, PriorityNode n2) {
+        int n1Index = n1.getIndex();
+        int n2Index = n2.getIndex();
+        PriorityNode temp = n1;
+        items[n1Index] = n2;
+        items[n2Index] = temp;
+        n1.setIndex(n2Index);
+        n2.setIndex(n1Index);
+    }
+
+    private void swim(PriorityNode node) {
+        if (parent(node) == null) {
+            return;
+        }
+        if (node.smaller(parent(node))) {
+            swap(node, parent(node));
+            swim(node);
+        } return;
+    }
+
+    private void sink(PriorityNode node) {
+        if (leftChild(node) == null && rightChild(node) == null) {
+            return;
+        }
+        if (node.smaller(leftChild(node)) && node.smaller(rightChild(node))) {
+            return;
+        }
+        if (leftChild(node).smaller(rightChild(node)) && leftChild(node).smaller(node)) {
+            swap(node, leftChild(node));
+            sink(node);
+        } else if (rightChild(node).smaller(leftChild(node)) && rightChild(node).smaller(node)) {
+            swap(node, rightChild(node));
+            sink(node);
+        } return;
+    }
+
+    @Override
+    public void add(T item, double priority) {
+        if (contains(item)) {
+            throw new IllegalArgumentException();
+        }
+        if (size == items.length - 1) {
+            resize(items.length * 2);
+        }
+        PriorityNode newNode = new PriorityNode(item, priority, end);
+        items[end] = newNode;
+        swim(newNode);
+        itemMap.put(item, newNode);
+        size += 1;
+        end += 1;
+    }
+
+    @Override
+    public boolean contains(T item) {
+        return itemMap.containsKey(item);
+    }
+
+    @Override
+    public T getSmallest() {
+        if (items[1] == null) {
+            throw new NoSuchElementException();
+        } return items[1].getItem();
+    }
+
+    @Override
+    public T removeSmallest() {
+        T smallest = getSmallest();
+        if (size / (items.length * 1.0) <= 0.25) {
+            resize(items.length/2);
+        }
+        swap(items[1], items[size]);
+        items[size] = null;
+        sink(items[1]);
+        size -= 1;
+        end -= 1;
+        itemMap.remove(smallest);
+        return smallest;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void changePriority(T item, double priority) {
+        if (!contains(item)) {
+            throw new NoSuchElementException();
+        }
+        PriorityNode node = itemMap.get(item);
+        boolean swim = priority < node.getPriority();
+        node.setPriority(priority);
+        if (swim) {
+            swim(node);
+        } else {
+            sink(node);
+        }
+    }
+}
--- proj2ab/bearmaps/ArrayHeapMinPQTest.java
+++ proj2ab/bearmaps/ArrayHeapMinPQTest.java
@@ -1,7 +1,179 @@
 package bearmaps;
 
+import edu.princeton.cs.algs4.Stopwatch;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
-public class ArrayHeapMinPQTest {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import static edu.princeton.cs.algs4.StdRandom.uniform;
+import static org.junit.Assert.*;
+
+public class ArrayHeapMinPQTest{
+
+    @Test
+    public void sanityCheck() {
+        ArrayHeapMinPQ arrayPQ = new ArrayHeapMinPQ();
+        NaiveMinPQ naivePQ = new NaiveMinPQ();
+        arrayPQ.add("item 1", 10);
+        naivePQ.add("item 1", 10);
+        assertEquals(arrayPQ.size(), naivePQ.size());
+        assertEquals(arrayPQ.getSmallest(), naivePQ.getSmallest());
+        arrayPQ.add("item 2", 2);
+        naivePQ.add("item 2", 2);
+        assertEquals(arrayPQ.size(), naivePQ.size());
+        assertEquals(arrayPQ.getSmallest(), naivePQ.getSmallest());
+        arrayPQ.changePriority("item 1", 1);
+        naivePQ.changePriority("item 1", 1);
+        assertEquals(arrayPQ.getSmallest(), naivePQ.getSmallest());
+        arrayPQ.removeSmallest();
+        naivePQ.removeSmallest();
+        assertEquals(arrayPQ.size(), naivePQ.size());
+        assertTrue(arrayPQ.contains("item 2"));
+        assertFalse(arrayPQ.contains("item 1"));
+    }
+
+    @Test
+    public void sanitySwim() {
+        ArrayHeapMinPQ arrayPQ = new ArrayHeapMinPQ();
+        NaiveMinPQ naivePQ = new NaiveMinPQ();
+        arrayPQ.add("item 5", 10);
+        naivePQ.add("item 5", 10);
+        arrayPQ.add("item 4", 9);
+        naivePQ.add("item 4", 9);
+        arrayPQ.add("item 3", 8);
+        naivePQ.add("item 3", 8);
+        arrayPQ.add("item 6", 12);
+        naivePQ.add("item 6", 12);
+        arrayPQ.add("item 7", 15);
+        naivePQ.add("item 7", 15);
+        arrayPQ.add("item 1", 3);
+        naivePQ.add("item 1", 3);
+        arrayPQ.add("item 2", 5);
+        naivePQ.add("item 2", 5);
+        assertEquals(arrayPQ.getSmallest(), naivePQ.getSmallest());
+    }
+
+    @Test
+    public void sanitySink() {
+        ArrayHeapMinPQ arrayPQ = new ArrayHeapMinPQ();
+        NaiveMinPQ naivePQ = new NaiveMinPQ();
+        arrayPQ.add("item 5", 10);
+        naivePQ.add("item 5", 10);
+        arrayPQ.add("item 4", 9);
+        naivePQ.add("item 4", 9);
+        arrayPQ.add("item 3", 8);
+        naivePQ.add("item 3", 8);
+        arrayPQ.add("item 6", 12);
+        naivePQ.add("item 6", 12);
+        arrayPQ.add("item 7", 15);
+        naivePQ.add("item 7", 15);
+        arrayPQ.add("item 1", 3);
+        naivePQ.add("item 1", 3);
+        arrayPQ.add("item 2", 5);
+        naivePQ.add("item 2", 5);
+        assertEquals(arrayPQ.removeSmallest(), naivePQ.removeSmallest());
+        assertEquals(arrayPQ.removeSmallest(), naivePQ.removeSmallest());
+        assertEquals(arrayPQ.getSmallest(), naivePQ.getSmallest());
+    }
+
+    @Test
+    public void removeSmallestRandom() {
+        ArrayHeapMinPQ arrayPQ = new ArrayHeapMinPQ();
+        NaiveMinPQ naivePQ = new NaiveMinPQ();
+
+        /* random addition of elements to both pqs */
+        for (int i = 0; i < 10000; i++) {
+            double temp = uniform(-500.0, 500.0);
+            arrayPQ.add(i, temp);
+            naivePQ.add(i, temp);
+        }
+
+        /* random removeSmallest calls to verify arrayPQ == naivePQ */
+        for (int i = 0; i < 200; i++) {
+            assertEquals(arrayPQ.removeSmallest(), naivePQ.removeSmallest());
+        }
+
+        /* changing priority of smallest & verifying */
+        for (int i = 0; i < 25; i++) {
+            double temp = uniform(-500.0, 500);
+            arrayPQ.changePriority(arrayPQ.getSmallest(), temp);
+            naivePQ.changePriority(naivePQ.getSmallest(), temp);
+            assertEquals(arrayPQ.getSmallest(), naivePQ.getSmallest());
+        }
+    }
+
+    private static void printTimingTable(List<Integer> listofNs, List<Double> times,
+                                         List<Integer> opCounts, String message) {
+        System.out.println(message);
+        System.out.printf("%12s %12s %12s %12s\n", "N", "time (s)", "# ops", "microsec/op");
+        System.out.printf("------------------------------------------------------------\n");
+        for (int i = 0; i < listofNs.size(); i += 1) {
+            int N = listofNs.get(i);
+            double time = times.get(i);
+            int opCount = opCounts.get(i);
+            double timePerOp = time / opCount * 1e6;
+            System.out.printf("%12d %12.2f %12d %12.2f\n", N, time, opCount, timePerOp);
+        }
+    }
+
+    @Test
+    public void timing() {
+        List<Integer> listofNs = List.of(31250, 62500, 125000, 250000, 500000, 1000000);
+        List<Double> constructionTimes = new ArrayList<>();
+        List<Double> removeSmallestTimes = new ArrayList<>();
+        List<Integer> removeSmallestOps = List.of(1000, 1000, 1000, 1000, 1000, 1000);
+
+        for (int N : listofNs) {
+            /* testing ArrayHeapMinPQ construction runtime */
+            Stopwatch constructSW = new Stopwatch();
+            ArrayHeapMinPQ arrayPQ = new ArrayHeapMinPQ();
+            for (int i = 0; i < N; i++) {
+                double temp = uniform(-500.0, 500.0);
+                arrayPQ.add(i, temp);
+            }
+            constructionTimes.add(constructSW.elapsedTime());
+
+            /* testing ArrayHeapMinPQ removeSmallest runtime */
+            Stopwatch removeSW = new Stopwatch();
+            for (int i = 0; i < 1000; i++) {
+                arrayPQ.removeSmallest();
+            }
+            removeSmallestTimes.add(removeSW.elapsedTime());
+        }
+
+        List<Double> naiveConstructionTimes = new ArrayList<>();
+        List<Double> naiveRemoveSmallestTimes = new ArrayList<>();
+        for (int N : listofNs) {
+            /* testing ArrayHeapMinPQ construction runtime */
+            Stopwatch naiveConstructSW = new Stopwatch();
+            NaiveMinPQ naivePQ = new NaiveMinPQ();
+            for (int i = 0; i < N; i++) {
+                double temp = uniform(-500.0, 500.0);
+                naivePQ.add(i, temp);
+            }
+            naiveConstructionTimes.add(naiveConstructSW.elapsedTime());
+
+            /* testing ArrayHeapMinPQ removeSmallest runtime */
+            Stopwatch naiveRemoveSW = new Stopwatch();
+            for (int i = 0; i < 1000; i++) {
+                naivePQ.removeSmallest();
+            }
+            naiveRemoveSmallestTimes.add(naiveRemoveSW.elapsedTime());
+        }
+
+        /* printing timing tables */
+        printTimingTable(listofNs, constructionTimes, listofNs,
+                "Timing table for ArrayHeapMinPQ Construction");
+        System.out.println();
+        printTimingTable(listofNs, removeSmallestTimes, removeSmallestOps,
+                "Timing table for ArrayHeapMinPQ removeSmallest");
+        System.out.println();
+        printTimingTable(listofNs, naiveConstructionTimes, listofNs,
+                "Timing table for NaiveMinPQ Construction");
+        System.out.println();
+        printTimingTable(listofNs, naiveRemoveSmallestTimes, removeSmallestOps,
+                "Timing table for NaiveMinPQ removeSmallest");
+    }
 }
