f1b4e5318be85a8e66126326404cafd55ce508e1
Sarah Varghese <svsarah01@berkeley.edu>
2020-11-06 13:38:49 -0600

    done with raster and part 1 of project

--- proj2d/bearmaps/proj2d/server/handler/impl/RasterAPIHandler.java
+++ proj2d/bearmaps/proj2d/server/handler/impl/RasterAPIHandler.java
@@ -17,8 +17,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static bearmaps.proj2d.utils.Constants.SEMANTIC_STREET_GRAPH;
-import static bearmaps.proj2d.utils.Constants.ROUTE_LIST;
+import static bearmaps.proj2d.utils.Constants.*;
 
 /**
  * Handles requests from the web browser for map images. These images
@@ -66,10 +65,10 @@ public class RasterAPIHandler extends APIRouteHandler<Map<String, Double>, Map<S
      *         <li>The tiles must be arranged in-order to reconstruct the full image.</li>
      *     </ul>
      *
-     * @param requestParams Map of the HTTP GET request's query parameters - the query box and
+     * requestParams Map of the HTTP GET request's query parameters - the query box and
      *               the user viewport width and height.
      *
-     * @param response : Not used by this function. You may ignore.
+     * response : Not used by this function. You may ignore.
      * @return A map of results for the front end as specified: <br>
      * "render_grid"   : String[][], the files to display. <br>
      * "raster_ul_lon" : Number, the bounding upper left longitude of the rastered image. <br>
@@ -82,14 +81,127 @@ public class RasterAPIHandler extends APIRouteHandler<Map<String, Double>, Map<S
      * "query_success" : Boolean, whether the query was able to successfully complete; don't
      *                    forget to set this to true on success! <br>
      */
+
+    private Integer[] cornersHelper(double lrlon, double ullon, double ullat, double lrlat, int i) {
+        double x_increment = Math.abs(ROOT_LRLON - ROOT_ULLON) / i;
+        double y_increment = Math.abs(ROOT_LRLAT - ROOT_ULLAT)/ i;
+
+        /* finding upper lon & lat of raster */
+        int x0 = -1;
+        int y0 = -1;
+        double lon = ROOT_ULLON;
+        double lat = ROOT_ULLAT;
+        while (ullon > lon) {
+            lon += x_increment;
+            x0 += 1;
+        }
+        while (ullat < lat) {
+            lat -= y_increment;
+            y0 += 1;
+        }
+
+        /* finding lower lon & lat of raster */
+        int x1 = i;
+        int y1 = i;
+        double bottomLon = ROOT_LRLON;
+        double bottomLat = ROOT_LRLAT;
+        while (lrlon < bottomLon) {
+            bottomLon -= x_increment;
+            x1 -= 1;
+        }
+        while (lrlat > bottomLat) {
+            bottomLat += y_increment;
+            y1 -= 1;
+        }
+
+        Integer[] corners = new Integer[]{x0, x1, y0, y1};
+        return corners;
+    }
+
+    private String[][] imagesConstructor(int depth, int x0, int x1, int y0, int y1) {
+        int x = x0;
+        int y = y0;
+        String[][] result = new String[y1 - y0 + 1][x1 - x0 + 1];
+        for (int i = 0; i < y1 - y0 + 1; i++) {
+            for (int j = 0; j < x1 - x0 + 1; j++) {
+                result[i][j] = "d" + depth + "_x" + x +"_y" + y +".png";
+                x += 1;
+            }
+            y += 1;
+            x = x0;
+        }
+        return result;
+    }
+
+    private Double[] latAndLon(int i, int x0, int x1, int y0, int y1) {
+        double x_increment = Math.abs(ROOT_LRLON - ROOT_ULLON) / i;
+        double y_increment = Math.abs(ROOT_LRLAT - ROOT_ULLAT)/ i;
+        Double[] result = new Double[4];
+        result[0] = ROOT_ULLON + (x_increment * x0);
+        result[1] = ROOT_ULLAT - (y_increment * y0);
+        result[2] = ROOT_ULLON + (x_increment * (x1 + 1));
+        result[3] = ROOT_ULLAT - (y_increment * (y1 + 1));
+        return result;
+    }
+
+    private boolean isValid(double lrlon, double ullon, double ullat, double lrlat) {
+        return !(ullon > ROOT_LRLON && ullat > ROOT_LRLAT || ullon > lrlon || ullat < lrlat);
+    }
+
     @Override
     public Map<String, Object> processRequest(Map<String, Double> requestParams, Response response) {
-        //System.out.println("yo, wanna know the parameters given by the web browser? They are:");
-        //System.out.println(requestParams);
-        Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented RasterAPIHandler.processRequest, nothing is displayed in "
-                + "your browser.");
-        return results;
+        double lrlon = requestParams.get("lrlon");
+        double ullon = requestParams.get("ullon");
+        double width = requestParams.get("w");
+        double height = requestParams.get("h");
+        double ullat = requestParams.get("ullat");
+        double lrlat = requestParams.get("lrlat");
+
+        /* checking for no coverage */
+        boolean success = isValid(lrlon, ullon, ullat, lrlat);
+
+        /* accounting for partial coverage */
+        ullon = Math.max(ROOT_ULLON, ullon);
+        ullat = Math.min(ROOT_ULLAT, ullat);
+        lrlon = Math.min(ROOT_LRLON, lrlon);
+        lrlat = Math.max(ROOT_LRLAT, lrlat);
+
+        /* calculating depth */
+        double queryLonDPP = (lrlon - ullon) / width;
+        int depth = 0;
+        double depthLonDPP = (ROOT_LRLON - ROOT_ULLON) / TILE_SIZE;
+        while (depthLonDPP > queryLonDPP) {
+            depthLonDPP /= 2.0;
+            depth += 1;
+        }
+        depth = Math.min(depth, 7);
+
+        /* n represents the num of rows/cols */
+        int n = (int) Math.sqrt(Math.pow(4, depth));
+
+        /* getting min & max x and y coordinates at depth */
+        Integer[] startAndEnd = cornersHelper(lrlon, ullon, ullat, lrlat, n);
+        int x0 = startAndEnd[0];
+        int x1 = startAndEnd[1];
+        int y0 = startAndEnd[2];
+        int y1 = startAndEnd[3];
+
+        /* getting images array */
+        String[][] raster = imagesConstructor(depth, x0, x1, y0, y1);
+
+        /* getting lon/lat coordinates of raster */
+        Double[] corners = latAndLon(n, x0, x1, y0, y1);
+
+        /* constructing Map to return */
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("render_grid", raster);
+        result.put("raster_ul_lon", corners[0]);
+        result.put("raster_ul_lat", corners[1]);
+        result.put("raster_lr_lon", corners[2]);
+        result.put("raster_lr_lat", corners[3]);
+        result.put("depth", depth);
+        result.put("query_success", success);
+        return result;
     }
 
     @Override
